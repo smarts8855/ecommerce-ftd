2d98571d4ac724f6e4fb2313c81f243f9128d667
smarts8855 <olaoyea64@gmail.com>
2025-01-21 15:12:04 +0100

    add coupon

---
+++ src/redux/slices/coupons/couponsSlice.js
@@ -0,0 +1,120 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import baseURL from "../../../utils/baseURL";
+import {
+  resetErrAction,
+  resetSuccessAction,
+} from "../globalActions/globalActions";
+
+//initialState
+const initialState = {
+  coupons: [],
+  coupon: null,
+  loading: false,
+  error: null,
+  isAdded: false,
+  isUpdated: false,
+  isDeleted: false,
+};
+
+//create coupon action
+export const createCouponAction = createAsyncThunk(
+  "coupons/create",
+  async (
+    { code, discount, startDate, endDate },
+    { rejectWithValue, getState, dispatch }
+  ) => {
+    try {
+      //Token- Authenticated
+      const token = getState()?.users?.userAuth?.userInfo?.token;
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      //Images
+      const { data } = await axios.post(
+        `${baseURL}/coupons`,
+        {
+          code,
+          discount,
+          startDate,
+          endDate,
+        },
+        config
+      );
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+//fetch coupon action
+export const fetchCouponsAction = createAsyncThunk(
+  "coupons/fetch All",
+  async (payload, { rejectWithValue, getState, dispatch }) => {
+    try {
+      // const { name } = payload;
+
+      //Images
+      const { data } = await axios.get(`${baseURL}/brands`);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+//slice
+const couponsSlice = createSlice({
+  name: "coupons",
+  initialState,
+  extraReducers: (builder) => {
+    //create
+    builder.addCase(createCouponAction.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(createCouponAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.coupon = action.payload;
+      state.isAdded = true;
+    });
+    builder.addCase(createCouponAction.rejected, (state, action) => {
+      state.loading = false;
+      state.coupon = null;
+      state.isAdded = false;
+      state.error = action.payload;
+    });
+
+    //fetch all
+    builder.addCase(fetchCouponsAction.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchCouponsAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.coupons = action.payload;
+      state.isAdded = true;
+    });
+    builder.addCase(fetchCouponsAction.rejected, (state, action) => {
+      state.loading = false;
+      state.coupons = null;
+      state.isAdded = false;
+      state.error = action.payload;
+    });
+    //reset error action
+    builder.addCase(resetErrAction.pending, (state, action) => {
+      state.isAdded = false;
+      state.error = null;
+    });
+    //reset success action
+    builder.addCase(resetSuccessAction.pending, (state, action) => {
+      state.isAdded = false;
+      state.error = null;
+    });
+  },
+});
+
+//generate the reducer
+const couponsReducer = couponsSlice.reducer;
+
+export default couponsReducer;
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -5,6 +5,7 @@ import categoryReducer from "../slices/categories/categoriesSlice";
 import brandsReducer from "../slices/categories/brandsSlice";
 import colorsReducer from "../slices/categories/colorsSlice";
 import cartReducer from "../slices/cart/cartSlices";
+import couponsReducer from "../slices/coupons/couponsSlice";
 
 //store
 
@@ -16,6 +17,7 @@ const store = configureStore({
     brands: brandsReducer,
     colors: colorsReducer,
     carts: cartReducer,
+    coupons: couponsReducer,
   },
 });
 
