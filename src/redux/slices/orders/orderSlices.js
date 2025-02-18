07db153f6a6760942c1ac9968130ea9930344053
smarts8855 <olaoyea64@gmail.com>
2025-02-18 14:03:12 +0100

    orders

---
+++ src/redux/slices/orders/orderSlices.js
@@ -0,0 +1,165 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import baseURL from "../../../utils/baseURL";
+import {
+  resetErrAction,
+  resetSuccessAction,
+} from "../globalActions/globalActions";
+
+//initialState
+const initialState = {
+  oders: [],
+  order: null,
+  loading: false,
+  error: null,
+  isAdded: false,
+  isUpdated: false,
+};
+
+//create product action
+export const placeOrderAction = createAsyncThunk(
+  "order/place-order",
+  async (payload, { rejectWithValue, getState, dispatch }) => {
+    try {
+      const { orderItems, shippingAddress, totalPrice } = payload;
+
+      //make request
+
+      //Token- Authenticated
+      const token = getState()?.users?.userAuth?.userInfo?.token;
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "multipart/form-data",
+        },
+      };
+
+      const { data } = await axios.post(
+        `${baseURL}/orders`,
+        {
+          orderItems,
+          shippingAddress,
+          totalPrice,
+        },
+        config
+      );
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+//fetch products action
+export const fetchProductsAction = createAsyncThunk(
+  "product/list",
+  async ({ url }, { rejectWithValue, getState, dispatch }) => {
+    console.log({ url });
+    try {
+      //make request
+
+      //Token- Authenticated
+      const token = getState()?.users?.userAuth?.userInfo?.token;
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const { data } = await axios.get(`${url}`, config);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+//fetch product action
+export const fetchProductAction = createAsyncThunk(
+  "product/details",
+  async (productId, { rejectWithValue, getState, dispatch }) => {
+    try {
+      //make request
+
+      //Token- Authenticated
+      const token = getState()?.users?.userAuth?.userInfo?.token;
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const { data } = await axios.get(
+        `${baseURL}/products/${productId}`,
+        config
+      );
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+//slice
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  extraReducers: (builder) => {
+    //create
+    builder.addCase(createProductAction.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(createProductAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.product = action.payload;
+      state.isAdded = true;
+    });
+    builder.addCase(createProductAction.rejected, (state, action) => {
+      state.loading = false;
+      state.product = null;
+      state.isAdded = false;
+      state.error = action.payload;
+    });
+    //fetch all
+    builder.addCase(fetchProductsAction.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchProductsAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.products = action.payload;
+      state.isAdded = true;
+    });
+    builder.addCase(fetchProductsAction.rejected, (state, action) => {
+      state.loading = false;
+      state.products = null;
+      state.isAdded = false;
+      state.error = action.payload;
+    });
+    //fetch single
+    builder.addCase(fetchProductAction.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchProductAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.product = action.payload;
+      state.isAdded = true;
+    });
+    builder.addCase(fetchProductAction.rejected, (state, action) => {
+      state.loading = false;
+      state.product = null;
+      state.isAdded = false;
+      state.error = action.payload;
+    });
+    // reset error
+    builder.addCase(resetErrAction.pending, (state, action) => {
+      state.error = null;
+    });
+    //reset success
+    builder.addCase(resetSuccessAction.pending, (state, action) => {
+      state.isAdded = false;
+    });
+  },
+});
+
+//generate the reducer
+const productReducer = productSlice.reducer;
+
+export default productReducer;
