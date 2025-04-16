1309be2682f4f74b49bded2823b01cbb78123035
smarts8855 <olaoyea64@gmail.com>
2025-04-16 10:38:49 +0100

    update base url

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,4 @@
-const baseURL = "http://localhost:7000/api/v1";
+// const baseURL = "http://localhost:7000/api/v1";
+const baseURL = "https://nodejs-api-ecommerce-bkd.onrender.com/api/v1";
 
 export default baseURL;
